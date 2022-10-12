482da0c7b66fe46413e70e210eb08921be2e627e
tomoyaaaaaa <20jz0122@jec.ac.jp>
2022-10-12 11:43:51 +0900

    1143

---
+++ resources/views/login.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Electronic Procedure</title>
+    <link rel="stylesheet" type="text/css" href="">
+    <!-- <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.css"> -->
+
+</head>
+<body>
+    <h1>Electronic Procedure</h1> 
+
+    <form action="ex06_02_2.php" method="post">
+        ID<input type="text" name="id"> 
+        パスワード<input type="password" name="pass">
+        
+        <br>
+                
+        <input class="blue" type="checkbox">パスワードを表示
+        <hr>
+        <button type="submit">次へ</button>                           
+    </form>
+    
+        <h4>Copyright ©ElectronicProcedure </h4>
+
+</body>
+</html>
